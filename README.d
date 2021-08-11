d2a2f751b1a2a5e47e61f0c7b0c262c71d0af727
sumaiya-antara <ttsta21@gmail.com>
2021-08-12 01:00:29 +0600

    Introduction added

---
+++ README.d
@@ -0,0 +1,6 @@
+## What is Python?
+Python is an object-oriented programming language. It provides rich data types and easier syntax than any other programming languages. Compared to other programming languages, it allows more run-time flexibility. 
+
+Python supports functional and structured programming also.
+
+
